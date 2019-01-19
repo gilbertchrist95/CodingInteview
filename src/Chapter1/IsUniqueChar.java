95141b2d4ec0f983209471d8209353e57874812c
Gilbert <gilbert@mokapos.com>
2019-01-19 14:35:41 +0700

    IsUniqueChars

---
+++ src/Chapter1/IsUniqueChar.java
@@ -0,0 +1,40 @@
+package Chapter1;
+
+public class IsUniqueChar {
+    public static void main(String[] args) {
+        String value = "Gilbert";
+        boolean isUnique = isUnique(value);
+        boolean isUnique2 = isUnique2(value);
+        System.out.println(isUnique);
+        System.out.println(isUnique2);
+
+        int x = 3 << 2;
+        System.out.println(x);
+    }
+
+    private static boolean isUnique2(String value) {
+        int checking = 0;
+        for (int i = 0; i < value.length(); i++) {
+            int charAt = value.charAt(i) - 'a';
+            if ((checking & (1 << charAt)) > 0) {
+                return false;
+            }
+            checking |= 1 << charAt;
+        }
+        return true;
+    }
+
+    private static boolean isUnique(String value) {
+        boolean[] chars = new boolean[256];
+        for (int i = 0; i < value.length(); i++) {
+            int charAt = value.charAt(i);
+            if (chars[charAt]) {
+                return false;
+            }
+            chars[charAt] = true;
+        }
+        return true;
+    }
+
+
+}
