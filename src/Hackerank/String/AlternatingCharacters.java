82a5fc9b16955438a4165b11f71907a3ffeadab6
Gilbert <gilbert@mokapos.com>
2019-02-10 20:07:35 +0700

    AlternatingCharacters.java

--- src/Hackerank/String/AlternatingCharacters.java
+++ src/Hackerank/String/AlternatingCharacters.java
@@ -1,4 +1,34 @@
 package Hackerank.String;
 
 public class AlternatingCharacters {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        int countTotal = 0;
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            int count = 0;
+            for (int j = i+1; j < s.length(); j++) {
+                if(c == s.charAt(j)){
+                    count++;
+                }else{
+                    break;
+                }
+            }
+            countTotal+=count;
+            i+=++count;
+        }
+        return countTotal;
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "BABABA";
+
+        int result = alternatingCharacters(s);
+        System.out.println(result);
+    }
+
 }
