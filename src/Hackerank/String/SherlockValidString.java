0b0dc3747cd85e09e4b00eb461f2955513851f69
Gilbert <gilbert@mokapos.com>
2019-02-10 20:07:51 +0700

    SherlockValidString

--- src/Hackerank/String/SherlockValidString.java
+++ src/Hackerank/String/SherlockValidString.java
@@ -1,4 +1,36 @@
 package Hackerank.String;
 
 public class SherlockValidString {
+    // Complete the isValid function below.
+    static String isValid(String s) {
+        int[] letter = new int[26];
+        for (char c : s.toCharArray()) {
+            int val = c - 'a';
+            if (letter[val] > 0) {
+                letter[val]--;
+            } else {
+                letter[val]++;
+            }
+        }
+
+        int count = 0;
+        for (int x : letter) {
+            count += x;
+            if (count > 1) {
+                System.out.println(count);
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "abcdefghhgfedecba";
+
+        String result = isValid(s);
+
+        System.out.println(result);
+    }
 }
