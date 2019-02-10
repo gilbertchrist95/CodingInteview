1dcf2942e1d2fb90b65a095bc5e8b5d9998cb04c
Gilbert <gilbert@mokapos.com>
2019-02-10 19:10:01 +0700

    Making Anagram

--- src/Hackerank/String/MakingAnagram.java
+++ src/Hackerank/String/MakingAnagram.java
@@ -1,4 +1,36 @@
 package Hackerank.String;
 
 public class MakingAnagram {
+    // Comple   te the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+        int[] letters = new int[26];
+
+        for (char c: a.toCharArray()){
+            letters[c-'a']++;
+        }
+
+        for (char c: b.toCharArray()){
+            letters[c-'a']--;
+        }
+
+        int sum = 0;
+
+        for (int x:letters){
+            sum+=Math.abs(x);
+        }
+        return sum;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        String a = "cde";
+
+        String b = "dcf";
+
+        int res = makeAnagram(a, b);
+
+        System.out.println(res);
+    }
 }
