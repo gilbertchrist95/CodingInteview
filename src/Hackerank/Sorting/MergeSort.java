6ab3f1350beb29cdbd76c2ac1e25b4e343188a7c
Gilbert <gilbert@mokapos.com>
2019-02-14 22:35:47 +0700

    Merge Sort

---
+++ src/Hackerank/Sorting/MergeSort.java
@@ -0,0 +1,54 @@
+package Hackerank.Sorting;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 9, 2, 8, 3, 7, 4, 6, 5, 0};
+        mergeSort(array);
+    }
+
+    private static void mergeSort(int[] array) {
+        mergeSort(array, new int[array.length], 0, array.length - 1);
+    }
+
+    private static void mergeSort(int[] array, int[] temp, int leftStart, int rightEnd) {
+        if (leftStart >= rightEnd) return;
+
+        int middle = (leftStart + rightEnd) / 2;
+        mergeSort(array, temp, leftStart, middle);
+        mergeSort(array, temp, middle + 1, rightEnd);
+        mergeHalves(array, temp, leftStart, rightEnd);
+    }
+
+    private static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd) {
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = (rightEnd - leftStart + 1);
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (array[left] <= array[right]) {
+                temp[index] = array[left];
+                left++;
+            } else {
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
+        }
+
+
+        System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(array, right, temp, index, rightEnd - right + 1);
+        System.arraycopy(temp, leftStart, array, leftStart, size);
+
+        for(int a: temp){
+            System.out.print(a+", ");
+        }
+        System.out.println();
+    }
+
+}
